a1ed633efeca41f067aed25e29cb4d9445e6503e
pablolopez101200@gmail.com <pablolopez101200@gmail.com>
2026-06-09 17:05:18 -0300

    feat: setup shadcn ui, custom navbar and dashboard layout

---
+++ components/navbar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { SignInButton, SignUpButton, UserButton, useUser } from '@clerk/nextjs'
+import { Button } from './ui/button';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+
+export default function Navbar() {
+    const { isSignedIn, user } = useUser();
+    const pathname = usePathname();
+
+    const isDashboardPage = pathname === "/dashboard";
+    const isBoardPage = pathname.startsWith("/boards/");
+
+    if (isDashboardPage) {
+        return (
+            <div>
+                <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+                    <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                        <div className="flex items-center space-x-2">
+                            <h1 className="text-xl sm:text-2xl font-bold text-gray-900">
+                                HUNOTASK
+                            </h1>
+                        </div>
+                       <UserButton />
+                    </div>
+                </header>
+            </div>
+        );
+    }
+
+    return (
+        <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+            <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                <div className="flex items-center space-x-2">
+                    <h1 className="text-xl sm:text-2xl font-bold text-gray-900">
+                        HUNOTASK
+                    </h1>
+                </div>
+                {isSignedIn ? (
+                    <div className="flex flex-col sm:flex-row items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4"> 
+                        <span className="text-xs sm:text-sm text-gray-600 hidden sm:block">
+                            Welcome, { user.firstName ?? user.emailAddresses[0].emailAddress}
+                        </span>
+                        <Link href="/dashboard">
+                            <Button size="sm" className='text-xs sm:text-sm'>
+                                Go to Dashboard <ArrowRight />
+                            </Button>
+                        </Link>
+                    </div>
+                ) : (    
+                    <div>
+                        <SignInButton>
+                            <Button variant="ghost" size="sm" className="text-xs sm:text-sm">
+                                Sign In
+                            </Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button size="sm" className="text-xs sm:text-sm">
+                                Sign Up
+                            </Button>
+                        </SignUpButton>
+                    </div>
+                )}
+           </div>
+        </header>
+    );
+}
